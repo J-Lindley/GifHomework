3c1afa9bf86fe78fbfe98ff43ad72f0b46568ff8
JLindley <jonlindley@gmail.com>
2019-04-29 17:30:47 -0700

    Got click to start working

--- javascript/apiproject.js
+++ javascript/apiproject.js
@@ -28,21 +28,21 @@ function renderButtons() {
 }
 
 // This function handles events where let me find it is clicked
-$("#add-topic").on("click", function(event) {
+$("#add-topic").on("click", function (event) {
   // event.preventDefault() prevents the form from trying to submit itself.
   // We're using a form so that the user can hit enter instead of clicking the button if they want
   event.preventDefault();
 
   // This line will grab the text from the input box
- var inputtedSearch =  $("#search-input").val();
- console.log($("#search-input").val())
+  var inputtedSearch = $("#search-input").val();
+  console.log($("#search-input").val())
 
   // The topic from the textbox is then added to our array
   topics.push(inputtedSearch);
-renderButtons();
-displayTopicInfo(inputtedSearch)
+  renderButtons();
+  displayTopicInfo(inputtedSearch)
   console.log(topics)
- 
+
   // calling renderButtons which handles the processing of our topic array
   //renderButtons();
 
@@ -53,9 +53,9 @@ renderButtons();
 
 //Make an event listener that gives displaytopicinfo
 
-$(".topic").on("click", function(){
+$(".topic").on("click", function () {
   var topic = $(this).attr("data-name");
- displayTopicInfo(topic)
+  displayTopicInfo(topic)
 })
 
 function displayTopicInfo(topic) {
@@ -66,34 +66,52 @@ function displayTopicInfo(topic) {
     url: queryURL,
     method: "GET"
   }).then(function (response) {
-console.log(response)
-console.log(response.data[0].images.downsized)
-console.log(queryURL)
-    for(var i = 0; i < response.data.length; i++){
+    console.log(response)
+    console.log(response.data[0].images.downsized)
+    console.log(queryURL)
+    for (var i = 0; i < response.data.length; i++) {
 
-    // Creating a div to hold the topic
-    var topicDiv = $("<div class='topic'>");
-    console.log(topicDiv)
-    // Storing the rating data
-    var rating = response.data[i].rating;
+      // Creating a div to hold the topic
+      var topicDiv = $("<div class='topic'>");
+      console.log(topicDiv)
+      // Storing the rating data
+      var rating = response.data[i].rating;
 
-    // Creating an element to have the rating displayed
-    var pOne = $("<p>").text("Rated: " + rating);
+      // Creating an element to have the rating displayed
+      var pOne = $("<p>").text("Rated: " + rating);
 
-    // Displaying the rating
-    topicDiv.append(pOne);
-    // Retrieving the URL for the image
-    var imgURL = response.data[i].images.downsized.url;
+      // Displaying the rating
+      topicDiv.append(pOne);
+      // Retrieving the URL for the image
+      var stillUrl = response.data[i].images.fixed_height_still.url
+      var animateUrl = response.data[i].images.fixed_height.url
 
-    // Creating an element to hold the image
-    var image1 = $("<img>").attr("src", imgURL);
-    topicDiv.prepend(image1)
-    $('#topic').prepend(topicDiv)
+      // Creating an element to hold the image
+      var image = $("<img class = 'gif'>").attr("src", stillUrl);
+      image.attr("data-still", stillUrl);
+      image.attr("data-animate", animateUrl);
+      image.attr("data-state", "still")
+      topicDiv.prepend(image)
+      $('#topic').prepend(topicDiv)
     }
   });
 
 }
 
+$(document).on("click", ".gif", function() {
+  // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+  var state = $(this).attr("data-state");
+  // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+  // Then, set the image's data-state to animate
+  // Else set src to the data-still value
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
 
 
 
